c3659c1c957690a3f9384daf8f9d7514b394911b
Robert Anisimov <skillup23@yandex.ru>
2026-01-12 12:35:58 +0300

    копирайт 2026

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,13 +1,13 @@
-import Link from "next/link";
-import InfoBanner from "./InfoBanner";
+import Link from 'next/link';
+import InfoBanner from './InfoBanner';
 
 export default function Footer() {
   return (
     <footer>
       <InfoBanner style="py-5">
-        <Link href={"/documents"}>
+        <Link href={'/documents'}>
           <p className="mx-auto w-full md:w-[905px] text-white text-sm md:text-xl text-center leading-6">
-            © СитиОйл 2025 Все права защищены.
+            © СитиОйл 2026 Все права защищены.
           </p>
         </Link>
       </InfoBanner>
